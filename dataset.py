0d124268b99b854d4c47febb7058a48095bb433d
John Armstrong <j.armstrong.2@research.gla.ac.uk>
2019-01-25 10:30:23 +0000

    Update to data.py & addition of dataset.py

---
+++ dataset.py
@@ -0,0 +1,68 @@
+import numpy as np
+from torch.utils.data import Dataset
+
+class solar_dataset(Dataset):
+    def __init__(self,source="from_file",dat_file=None,data_arr=None,label_arr=None):
+        super(solar_dataset,self).__init__()
+        if source == "from_file":
+            if dat_file is None:
+                raise TypeError("dat_file should not be done when initialising a class instance from a file!")
+            f = np.load(dat_file)
+            if len(f.files) == 2:
+                self.header = f["header"]
+            self.label = f["data"][:,0]
+            self.length = f["data"].shape[0]
+            self.data = f["data"][:,1:].reshape(self.length,1,256,256)
+            del f
+        elif source == "numpy":
+            self.label = label_arr
+            self.length = label_arr.shape[0]
+            self.data = data_arr.reshape(self.length,1,256,256)
+        else:
+            raise TypeError("Invalid source format.")
+
+    def __len__(self):
+        return len(self.label)
+
+    def __getitem__(self,idx):
+        if hasattr(self,"header"):
+            item = self.data[idx]
+            label = self.label[idx]
+            header = self.header[idx]
+
+            return (item,label,header)
+        else:
+            item = self.data[idx]
+            label = self.label[idx]
+
+            return (item,label)
+
+def parse_dataset(dataset):
+    '''
+    A function for splitting the dataset into the different classes for use in the confusion matrix.
+
+    Parameters
+    ----------
+    dataset : solar_dataset or str
+        A solar dataset class instance or path to the file containing images of the different classes.
+
+    Returns
+    -------
+    sol_list : list
+        A list of solar_dataset instances with dependent length on the length of np.unique(dataset.label).
+    '''
+
+    sol_list = []
+    
+    if type(dataset) == solar_dataset:
+        for l in np.unique(dataset.label):
+            args = np.argwhere(dataset.label == l) #finds all of the indices where the given label is l and returns an array of these indices
+    
+            sol_list.append(solar_dataset(source="numpy",data_arr=dataset.data[args],label_arr=dataset.label[args]))
+    elif type(dataset) == str:
+        s_dataset = solar_dataset(source="from_file",dat_file=dataset)
+
+        for l in np.unique(s_dataset.label):
+            args = np.argwhere(s_dataset.label == l)
+
+            sol_list.append(solar_dataset(source="numpy",data_arr=s_dataset.data[args],label_arr=s_dataset.label[args]))
