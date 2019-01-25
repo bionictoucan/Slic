41db1231e47d1a313b28b068454b8fd40916ab73
John Armstrong <j.armstrong.2@research.gla.ac.uk>
2019-01-25 11:02:08 +0000

    Addition of model.py

---
+++ model.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+from torch.nn.init import kaiming_normal_
+
+class solar_classifier(nn.Module):
+    def __init__(self):
+        super(solar_classifier,self).__init__()
+        self.max_pool = nn.MaxPool2d(kernel_size=2,stride=2)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1,64,kernel_size=3,padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64,64,kernel_size=3,padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64,128,kernel_size=3,padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128,128,kernel_size=3,padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128,256,kernel_size=3,padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(256,256,kernel_size=3,padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(256,512,kernel_size=3,padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(512,512,kernel_size=3,padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(512*8*8,4096),    
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096,4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096,5)
+        )
+
+        for m in self.modules():
+            if not solar_classifier:
+                kaiming_normal_(m.weight,nonlinearity="relu")
+
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.maxPool(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.maxPool(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.maxPool(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.maxPool(out)
+        out = self.layer8(out)
+        out = self.layer8(out)
+        out = self.maxPool(out)
+        out = out.view(out.size(0),-1)
+        out = self.classifier(out)
+
+    def graph(self):
+        return nn.Sequential(self.layer1,self.layer2,self.maxPool,self.layer3,self.layer4,self.maxPool,self.layer5,self.layer6,self.maxPool,self.layer7,self.layer8, self.maxPool,self.layer8,self.layer8,self.maxPool,self.classifier)
